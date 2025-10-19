91509cc960c0ecb98d71f928ff0ac0bc6600a6a6
pandochkaa <v.alyashkuratova@yandex.ru>
2025-10-19 17:19:02 +0300

    feat: add FASTA analyzer with sequence counting and mean length calculation

---
+++ bio_analyzer/abstract.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Iterator
+from .record import SequenceRecord
+
+class SequenceReader(ABC):
+    """Абстрактный базовый класс для чтения последовательностей."""
+    
+    def __init__(self, filepath: str | Path):
+        self.filepath = Path(filepath)
+    
+    @abstractmethod
+    def read(self) -> Iterator[SequenceRecord]:
+        """Чтение последовательностей."""
+        pass
+    
+    @abstractmethod
+    def get_seq_count(self) -> int:
+        """Получить количество последовательностей."""
+        pass
+    
+    @abstractmethod
+    def get_mean_seq_length(self) -> float:
+        """Получить среднюю длину последовательностей."""
+        pass
